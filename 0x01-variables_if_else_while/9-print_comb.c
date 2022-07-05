332522a6058aa45c3e373cbb089c4fed644fe32d
kandadeken <dkokraku@gmail.com>
2022-07-04 22:33:29 -0700

    all possible

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations
+ * of single digit numbers
+ * Return: always return 0
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar (ch);
+if (ch != 57)
+{
+putchar (44);
+putchar (32);
+}
+
+return (0);
+}
