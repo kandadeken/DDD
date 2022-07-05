405f5a4af0159a44fef3263dea63f55b5d5dc3ca
kandadeken <dkokraku@gmail.com>
2022-07-04 17:06:22 -0700

    if else in c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* main ->  assign a random number to the variable n 
+* main - assign a random number to the variable n
 * each time it is executed and prints out based on condition
 * Return : always 0
 */
