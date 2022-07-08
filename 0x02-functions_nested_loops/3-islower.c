d08d9adb633325da3baf03441e8a8a0c8b89c8ce
kandadeken <kandadeken@github.com>
2022-07-08 02:58:36 +0000

    nesty

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+# include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: a character argument
+ * Return: 1 if c is lowercase and 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+
+}
